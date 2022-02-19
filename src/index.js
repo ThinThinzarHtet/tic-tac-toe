b8252ef38751badc73a4305d7fc99a36cf7e1479
ThinThinzarHtet <thinthinzarhtet19@gmail.com>
2022-02-19 18:11:22 +0630

    fix error

--- src/index.js
+++ src/index.js
@@ -58,11 +58,11 @@ class Game extends React.Component {
     }
 
     handleClick(i) {
-        const history = this.state.history.splice(0, this.state.stepNumber + 1);
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
         const current = history[history.length - 1];
         const squares = current.squares.slice();
         if (calculateWinner(squares) || squares[i]) {
-            return;
+            return; // can't be able to click if we get a winner
         }
         squares[i] = this.state.xIsNext ? 'X' : 'O';
         this.setState({
